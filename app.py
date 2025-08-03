cd78c6721f0b46d304736490bc0c41b71981e391
TKM-h.almughrabi-c <h.almughrabi-c@takamol.sa>
2025-08-03 16:06:41 +0300

    Add uvicorn server

--- app.py
+++ app.py
@@ -2151,5 +2151,5 @@ if __name__ == '__main__':
         db.create_all()
         # Handle migration for existing databases
         migrate_database()
-    app.run(debug=True, port=5001)
+    app.run(debug=True)
 
